6984fcb436bd04d1fe32000c7dc22cfca78af69f
Adam Hamar <adam.hamar@gmail.com>
2023-02-15 19:12:21 +0100

    add base font and colors to the tailwind config

--- qr-code-component-main/tailwind/tailwind.config.js
+++ qr-code-component-main/tailwind/tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
     content: ["./index.html"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Outfit", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                back: "hsl(212, 45%, 89%)",
+                title: "hsl(218, 44%, 22%)",
+                desc: "hsl(220, 15%, 55%)",
+            },
+        },
     },
-    plugins: [],
+    plugins: [
+        plugin(function ({ addBase }) {
+            addBase({
+                html: { fontSize: "15px" },
+            });
+        }),
+    ],
 };
