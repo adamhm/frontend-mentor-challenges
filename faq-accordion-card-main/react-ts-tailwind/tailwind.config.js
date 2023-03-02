dd15095dc2e7cc92d6141b47ec42b3ecaba33123
Adam Hamar <adam.hamar@gmail.com>
2023-03-02 20:08:26 +0100

    add colors and fonts to the tailwind config

--- faq-accordion-card-main/react-ts-tailwind/tailwind.config.js
+++ faq-accordion-card-main/react-ts-tailwind/tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Kumbh Sans", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "gradient-soft-violet":
+                    "hsl(var(--color-gradient-soft-violet))",
+                "gradient-soft-blue": "hsl(var(--color-gradient-soft-blue))",
+                card: "hsl(var(--color-card-bg))",
+                divider: "hsl(var(--color-divider))",
+                text1: "hsl(var(--color-text1))",
+                text2: "hsl(var(--color-text2))",
+                hover: "hsl(var(--color-hover))",
+            },
+        },
     },
     plugins: [],
 };
