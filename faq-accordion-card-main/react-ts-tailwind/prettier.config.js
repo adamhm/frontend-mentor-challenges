870a9b4813848a12b337df7926f745da1c38d3e0
Adam Hamar <adam.hamar@gmail.com>
2023-03-03 19:11:56 +0100

    add prettier to the project

---
+++ faq-accordion-card-main/react-ts-tailwind/prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: [require("prettier-plugin-tailwindcss")],
+    tailwindConfig: "./tailwind.config.js",
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: false,
+}
