668bca757de180001313e392f46a49eeea925fd7
Adam Hamar <adam.hamar@gmail.com>
2023-03-03 19:38:56 +0100

    refactor the code of the background gradient

--- faq-accordion-card-main/react-ts-tailwind/tailwind.config.js
+++ faq-accordion-card-main/react-ts-tailwind/tailwind.config.js
@@ -1,25 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     theme: {
         extend: {
             fontFamily: {
                 sans: ["Kumbh Sans", ...defaultTheme.fontFamily.sans],
             },
             colors: {
-                "gradient-soft-violet":
-                    "hsl(var(--color-gradient-soft-violet))",
-                "gradient-soft-blue": "hsl(var(--color-gradient-soft-blue))",
+                "soft-violet": "hsl(var(--color-soft-violet))",
+                "soft-blue": "hsl(var(--color-soft-blue))",
                 card: "hsl(var(--color-card-bg))",
                 divider: "hsl(var(--color-divider))",
                 text1: "hsl(var(--color-text1))",
                 text2: "hsl(var(--color-text2))",
                 hover: "hsl(var(--color-hover))",
             },
+            backgroundImage: {},
         },
     },
     plugins: [],
-};
+}
