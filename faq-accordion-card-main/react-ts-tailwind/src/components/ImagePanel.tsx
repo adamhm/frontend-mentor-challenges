a17360a7365b089c98053923ff41c2865f2879aa
Adam Hamar <adam.hamar@gmail.com>
2023-03-06 01:46:14 +0100

    add ImagePanel component

---
+++ faq-accordion-card-main/react-ts-tailwind/src/components/ImagePanel.tsx
@@ -0,0 +1,7 @@
+function ImagePanel() {
+    return (
+        <div className="grow bg-desktop bg-pos-desktop bg-no-repeat before:relative before:top-[210px] before:inline-block before:h-[184px] before:w-[191px] before:bg-box-desktop before:bg-cover before:bg-center before:[left:-93px]"></div>
+    );
+}
+
+export default ImagePanel;
