3d8cce7d91ea6b64e2cf490867e194f471be7ca0
Adam Hamar <adam.hamar@gmail.com>
2023-03-06 10:57:46 +0100

    add shadows to the Card component

--- faq-accordion-card-main/react-ts-tailwind/src/components/Card.tsx
+++ faq-accordion-card-main/react-ts-tailwind/src/components/Card.tsx
@@ -4,7 +4,7 @@ import accordions from "../shared/accordions";
 
 function Card() {
     return (
-        <article className="flex h-full">
+        <article className="flex h-full shadow-2xl">
             <ImagePanel />
             <AccordionContainer title="FAQ" accordions={accordions} />
         </article>
