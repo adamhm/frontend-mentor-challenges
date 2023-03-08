2133428366d13c49aa18771d74fcf4c4cc2370ea
Adam Hamar <adam.hamar@gmail.com>
2023-03-08 11:43:52 +0100

    fix the item outline in expanded state

--- faq-accordion-card-main/react-ts-tailwind/src/components/AccordionItem.tsx
+++ faq-accordion-card-main/react-ts-tailwind/src/components/AccordionItem.tsx
@@ -14,10 +14,10 @@ function AccordionItem({
     itemClicked,
 }: Props) {
     return (
-        <div className="border-b border-solid border-b-divider text-[14px]">
+        <div className="border-b border-solid border-b-divider text-[14px] focus-within:outline focus-within:outline-2 focus-within:outline-black">
             <h2>
                 <button
-                    className="flex w-full cursor-pointer items-center border-none bg-transparent px-0 py-[1.1rem] text-[13.333px] font-normal leading-[normal] after:ml-auto after:mr-[0.9rem] after:h-[7px] after:w-[10px] after:bg-white after:bg-arrow after:bg-cover after:bg-center after:bg-no-repeat after:content-[''] aria-expanded:font-bold aria-expanded:after:rotate-180 aria-[expanded=false]:hover:text-hover"
+                    className="flex w-full cursor-pointer items-center border-none bg-transparent px-0 py-[1.1rem] text-[13.333px] font-normal leading-[normal] outline-none after:ml-auto after:mr-[0.9rem] after:h-[7px] after:w-[10px] after:bg-white after:bg-arrow after:bg-cover after:bg-center after:bg-no-repeat after:content-[''] aria-expanded:font-bold aria-expanded:after:rotate-180 aria-[expanded=false]:hover:text-hover"
                     id={`question${id}`}
                     aria-controls={`answer${id}`}
                     aria-expanded={expanded}
