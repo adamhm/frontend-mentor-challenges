b53ecdb3e1929f12de8bd4028f09567272a41f65
Adam Hamar <adam.hamar@gmail.com>
2023-03-14 12:46:26 +0100

    add theme colors, fonts and images

--- testimonials-grid-section-main/tailwind/tailwind.config.js
+++ testimonials-grid-section-main/tailwind/tailwind.config.js
@@ -3,9 +3,33 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    content: [],
+    content: ["./index.html"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: [
+                    "Barlow Semi Condensed",
+                    ...defaultTheme.fontFamily.sans,
+                ],
+            },
+            colors: {
+                desktop: "hsl(var(--color-desktop))",
+                bg1: "hsl(var(--color-bg1))",
+                bg2: "hsl(var(--color-bg2))",
+                bg3: "hsl(var(--color-bg3))",
+                bg4: "hsl(var(--color-bg4))",
+                light: "hsl(var(--color-text-light))",
+                dark: "hsl(var(--color-text-dark))",
+                item1: "hsl(var(--color-item1))",
+                item2: "hsl(var(--color-item2))",
+                item3: "hsl(var(--color-item3))",
+                item4: "hsl(var(--color-item4))",
+                purple: "hsl(var(--color-bg1))",
+            },
+            backgroundImage: {
+                "img-quotation": "url('../images/bg-pattern-quotation.svg')",
+            },
+        },
     },
     plugins: [],
 };
