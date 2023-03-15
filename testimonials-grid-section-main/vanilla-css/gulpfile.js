e8eaa62aae1f8787a6a7836c11477659168f5b17
Adam Hamar <adam.hamar@gmail.com>
2023-03-16 00:52:16 +0100

    rewrite the gulp tasks

--- testimonials-grid-section-main/vanilla-css/gulpfile.js
+++ testimonials-grid-section-main/vanilla-css/gulpfile.js
@@ -1,19 +1,14 @@
-var gulp = require("gulp");
-var sass = require("gulp-sass")(require("sass"));
-var print = require("gulp-print").default;
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const print = require("gulp-print").default;
 
-gulp.task("sass", function (cb) {
-    gulp.src("./src/*.scss")
+function buildStyle() {
+    return src("./src/*.scss")
         .pipe(print())
         .pipe(sass({ indentWidth: 4 }).on("error", sass.logError))
-        .pipe(gulp.dest("./styles"));
-    cb();
-});
+        .pipe(dest("./styles"));
+}
 
-gulp.task(
-    "default",
-    gulp.series("sass", function (cb) {
-        gulp.watch("./src/*.scss", gulp.series("sass"));
-        cb();
-    })
-);
+exports.default = function () {
+    watch("./src/*.scss", buildStyle);
+};
