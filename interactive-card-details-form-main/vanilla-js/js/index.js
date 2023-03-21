60ad11b1f18396332a2d854a8db2a5fe92aa9fbe
Adam Hamar <adam.hamar@gmail.com>
2023-03-21 20:01:20 +0100

    add javascript code to update the card view

---
+++ interactive-card-details-form-main/vanilla-js/js/index.js
@@ -0,0 +1,36 @@
+(function () {
+    const cardForm = document.getElementById("card_form");
+    const cardNumberField = document.getElementById("card_number");
+    const cardNameField = document.getElementById("card_name");
+    const cardExpirationField = document.getElementById("card_expiration");
+    const cvcField = document.getElementById("card_cvc");
+
+    const formElements = Array.from(cardForm.elements);
+    formElements.forEach((elem) => elem.addEventListener("input", update));
+
+    function update() {
+        cardForm.cardnumber.value = formatCardNumber(cardForm.cardnumber.value);
+        cardNumberField.innerText = cardForm.cardnumber.value;
+        cardNameField.innerText = cardForm.name.value;
+        cardExpirationField.innerText = `${cardForm.month.value}/${cardForm.year.value}`;
+        cvcField.innerText = cardForm.cvc.value;
+    }
+
+    function formatCardNumber(cardNumber) {
+        cardNumber = cardNumber.replaceAll(" ", "");
+
+        if (cardNumber.length === 0) return "";
+
+        let formattedNumber = cardNumber[0];
+
+        for (let i = 1; i < cardNumber.length; i++) {
+            if (i % 4 === 0) {
+                formattedNumber += " ";
+            }
+
+            formattedNumber += cardNumber[i];
+        }
+
+        return formattedNumber.toUpperCase();
+    }
+})();
