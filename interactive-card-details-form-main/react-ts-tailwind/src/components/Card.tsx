60ab16a0231e75d43bc2f489d5860b498ee7606e
Adam Hamar <adam.hamar@gmail.com>
2023-03-29 13:03:48 +0200

    add card component

---
+++ interactive-card-details-form-main/react-ts-tailwind/src/components/Card.tsx
@@ -0,0 +1,13 @@
+import ImagePanel from "./ImagePanel";
+import CardForm from "./CardForm";
+
+function Card() {
+    return (
+        <article>
+            <ImagePanel />
+            <CardForm />
+        </article>
+    );
+}
+
+export default Card;
