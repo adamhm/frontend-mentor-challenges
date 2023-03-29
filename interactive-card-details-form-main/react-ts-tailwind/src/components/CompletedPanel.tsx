e8379552c971964467dafdedbbca640fa1769960
Adam Hamar <adam.hamar@gmail.com>
2023-03-29 13:04:47 +0200

    add completedpanel component

---
+++ interactive-card-details-form-main/react-ts-tailwind/src/components/CompletedPanel.tsx
@@ -0,0 +1,12 @@
+function CompletedPanel() {
+    return (
+        <div id="completed">
+            <img src="images/icon-complete.svg" width="82" height="82" alt="" />
+            <p>Thank you!</p>
+            <p>We've added your card details</p>
+            <button type="button">Continue</button>
+        </div>
+    );
+}
+
+export default CompletedPanel;
