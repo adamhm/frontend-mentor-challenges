aa55fbe28712e1f39f646091824e9eeddcb27c88
Adam Hamar <adam.hamar@gmail.com>
2023-03-31 11:05:23 +0200

    add style rules to the completedpanel component

--- interactive-card-details-form-main/react-ts-tailwind/src/components/CompletedPanel.tsx
+++ interactive-card-details-form-main/react-ts-tailwind/src/components/CompletedPanel.tsx
@@ -1,10 +1,19 @@
 function CompletedPanel() {
     return (
-        <div id="completed">
+        <div id="completed" className="mt-[300px] hidden w-[381px] text-center">
             <img src="images/icon-complete.svg" width="82" height="82" alt="" />
-            <p>Thank you!</p>
-            <p>We've added your card details</p>
-            <button type="button">Continue</button>
+            <p className="text-completed-text mb-4 mt-8 text-[28px] uppercase leading-[normal] tracking-[3px]">
+                Thank you!
+            </p>
+            <p className="text-completed-text-2 text-[18px] leading-[normal]">
+                We've added your card details
+            </p>
+            <button
+                type="button"
+                className="mt-12 h-[53px] w-full rounded-[8px] bg-button text-[18px] leading-[normal] text-button-text"
+            >
+                Continue
+            </button>
         </div>
     );
 }
