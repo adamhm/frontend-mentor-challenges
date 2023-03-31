6e9be7f405353ecd2228db041423582fd275f59c
Adam Hamar <adam.hamar@gmail.com>
2023-03-31 11:04:22 +0200

    add tailwind css configurations

--- interactive-card-details-form-main/react-ts-tailwind/tailwind.config.js
+++ interactive-card-details-form-main/react-ts-tailwind/tailwind.config.js
@@ -5,7 +5,41 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Space Grotesk", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                card: "var(--color-card)",
+                "card-front": "var(--color-card-front)",
+                "card-back": "var(--color-card-back)",
+                label: "var(--color-label)",
+                "input-border": "var(--color-input-border)",
+                "input-placeholder": "var(--color-input-placeholder)",
+                "input-focus": "var(--color-input-focus)",
+                error: "var(--color-error)",
+                button: "var(--color-button)",
+                "button-text": "var(--color-button-text)",
+                "completed-text": "var(--color-completed-text-1",
+                "completed-text-2": "var(--color-completed-text-2",
+            },
+            backgroundImage: {
+                "main-desktop": "var(--image-main-desktop)",
+                "image-front":
+                    "var(--image-card-logo), var(--image-card-front)",
+                "image-card-back": "var(--image-card-back)",
+                "main-mobile": "var(--image-main-mobile)",
+            },
+            backgroundPosition: {
+                "position-front": "30px 26px, top left",
+            },
+            width: {
+                container: "1440px",
+            },
+            height: {
+                container: "900px",
+            },
+        },
     },
     plugins: [],
 };
