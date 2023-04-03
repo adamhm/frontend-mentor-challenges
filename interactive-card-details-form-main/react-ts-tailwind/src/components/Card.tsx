46e9729fbfdbc4433c06cc0481f966172f7da4f2
Adam Hamar <adam.hamar@gmail.com>
2023-04-03 23:48:23 +0200

    add bank card state

--- interactive-card-details-form-main/react-ts-tailwind/src/components/Card.tsx
+++ interactive-card-details-form-main/react-ts-tailwind/src/components/Card.tsx
@@ -1,11 +1,30 @@
+import { FormEvent, useState } from "react";
+
 import ImagePanel from "./ImagePanel";
 import CardForm from "./CardForm";
+import { BankCard } from "../shared/BankCard";
+
+const defaultCard: BankCard = {
+    name: "Jane Appleseed",
+    number: "0000 0000 0000 0000",
+    month: "00",
+    year: "00",
+    cvc: "000",
+};
 
 function Card() {
+    const [bankCard, setBankCard] = useState<BankCard>(defaultCard);
+
+    const handleDataChange = (event: FormEvent) => {
+        const target = event.target as HTMLInputElement;
+
+        setBankCard({ ...bankCard, [target.name]: target.value });
+    };
+
     return (
         <article className="flex h-full justify-center bg-card">
-            <ImagePanel />
-            <CardForm />
+            <ImagePanel bankCard={bankCard} />
+            <CardForm onDataChange={handleDataChange} />
         </article>
     );
 }
