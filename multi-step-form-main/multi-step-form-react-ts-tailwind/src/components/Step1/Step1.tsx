6a8ac9389642b5a9b567ccde0119b8b320a29ed3
Adam Hamar <adam.hamar@gmail.com>
2023-06-03 11:03:34 +0200

    create the Step1 component

---
+++ multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/Step1/Step1.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { StepTitleBar } from "@components";
+import { useId } from "react";
+
+function Step1() {
+    const nameId = useId();
+    const emailId = useId();
+    const phoneId = useId();
+
+    return (
+        <div>
+            <StepTitleBar
+                title="Personal info"
+                subtitle="Please provide your name, email address, and phone number."
+            />
+            <form action="" className="mt-[34px]">
+                <label
+                    htmlFor={nameId}
+                    className="block font-medium text-marine-blue"
+                >
+                    Name
+                </label>
+                <input
+                    id={nameId}
+                    name="name"
+                    type="text"
+                    placeholder="e.g. Stephen King"
+                    className="mt-1 block h-[3rem] w-full rounded-lg border border-gray-300 p-4 placeholder:font-medium"
+                />
+                <label htmlFor={emailId} className="mt-5 block font-medium">
+                    Email Address
+                </label>
+                <input
+                    id={emailId}
+                    type="text"
+                    placeholder="e.g. stephenking@lorem.com"
+                    className="mt-1 block h-[3rem] w-full rounded-lg border border-gray-300 p-4 placeholder:font-medium"
+                />
+                <label htmlFor={phoneId} className="mt-5 block font-medium">
+                    Phone Number
+                </label>
+                <input
+                    id={phoneId}
+                    type="text"
+                    placeholder="e,g, +1 234 567 890"
+                    className="mt-1 block h-[3rem] w-full rounded-lg border border-gray-300 p-4 placeholder:font-medium"
+                />
+            </form>
+            <button
+                type="button"
+                className="ml-auto mt-[92px] block h-[48px] w-[124px] rounded-md bg-marine-blue text-white"
+            >
+                Next Step
+            </button>
+        </div>
+    );
+}
+
+export default Step1;
