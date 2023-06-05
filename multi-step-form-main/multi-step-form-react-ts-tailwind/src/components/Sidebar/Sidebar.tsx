013bc63c3fa17f66da5711895c9b65d3a60bd81a
Adam Hamar <adam.hamar@gmail.com>
2023-06-05 20:10:37 +0200

    fix: remove margin from the Sidebar

--- multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/Sidebar/Sidebar.tsx
+++ multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/Sidebar/Sidebar.tsx
@@ -2,7 +2,7 @@ import { StepPanel } from "@components";
 
 function Sidebar() {
     return (
-        <section className="m-4 h-[568px] w-[274px] bg-desktop bg-no-repeat">
+        <section className="h-[568px] w-[274px] bg-desktop bg-no-repeat">
             <StepPanel />
         </section>
     );
