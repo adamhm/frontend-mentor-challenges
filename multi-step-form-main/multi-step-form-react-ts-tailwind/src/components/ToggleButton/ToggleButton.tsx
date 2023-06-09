441944d3497ba19c9b0cab573230d754143a36b2
Adam Hamar <adam.hamar@gmail.com>
2023-06-09 12:12:10 +0200

    simplify conditional styling

--- multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/ToggleButton/ToggleButton.tsx
+++ multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/ToggleButton/ToggleButton.tsx
@@ -1,23 +1,34 @@
+import { TWS } from "tailwind-fun";
+
 type ToggleButtonProps = {
     state: "monthly" | "yearly";
     onChange?: (state: "monthly" | "yearly") => void;
 };
 
 function ToggleButton({ state, onChange }: ToggleButtonProps) {
-    const leftColor =
-        state === "monthly" ? "text-marine-blue" : "text-cool-gray";
-    const rightColor =
-        state === "yearly" ? "text-marine-blue" : "text-cool-gray";
+    const spanStyle = TWS("mr-6 font-medium").addIfElse(
+        state === "monthly",
+        "text-cool-gray",
+        "text-marine-blue"
+    );
+
+    const spanStyleRight = TWS("ml-6 font-medium").addIfElse(
+        state === "monthly",
+        "text-marine-blue",
+        "text-cool-gray"
+    );
+
+    const buttonStyle = TWS(
+        "inline-flex h-[20px] w-[36px] items-center rounded-full bg-marine-blue p-1"
+    ).addWhen(state === "yearly", "justify-end");
 
     return (
         <div>
-            <span className={`mr-6 font-medium ${leftColor}`}>Monthly</span>
+            <span className={spanStyle.className}>Monthly</span>
             <button
                 type="button"
                 aria-pressed={state === "yearly"}
-                className={`inline-flex h-[20px] w-[36px] items-center ${
-                    state === "yearly" && "justify-end"
-                } rounded-full bg-marine-blue p-1`}
+                className={buttonStyle.className}
                 onClick={() =>
                     onChange?.(state === "monthly" ? "yearly" : "monthly")
                 }
@@ -28,7 +39,7 @@ function ToggleButton({ state, onChange }: ToggleButtonProps) {
                     className="h-[12px] w-[12px] rounded-full bg-white"
                 />
             </button>
-            <span className={`ml-6 font-medium ${rightColor}`}>Yearly</span>
+            <span className={spanStyleRight.className}>Yearly</span>
         </div>
     );
 }
