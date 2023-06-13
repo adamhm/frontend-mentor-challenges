44a6c8eca15f19a05faa0953146eaaf20ed4798d
Adam Hamar <adam.hamar@gmail.com>
2023-06-13 20:12:56 +0200

    create the reducer function

---
+++ multi-step-form-main/multi-step-form-react-ts-tailwind/src/state/form-reducer.ts
@@ -0,0 +1,30 @@
+import { FormState, FormAction } from "@typedefs";
+
+function formReducer(state: FormState, action: FormAction): FormState {
+    switch (action.type) {
+        case "ADD_ADDON":
+            return {
+                ...state,
+                addons: { ...state.addons, [action.payload]: true },
+            };
+        case "REMOVE_ADDON":
+            return {
+                ...state,
+                addons: { ...state.addons, [action.payload]: false },
+            };
+        case "SET_BILLING":
+            return { ...state, billing: action.payload };
+        case "SET_EMAIL":
+            return { ...state, email: action.payload };
+        case "SET_NAME":
+            return { ...state, name: action.payload };
+        case "SET_PHONE":
+            return { ...state, phone: action.payload };
+        case "SET_PLAN":
+            return { ...state, plan: action.payload };
+        default:
+            return state;
+    }
+}
+
+export default formReducer;
