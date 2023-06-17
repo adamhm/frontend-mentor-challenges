eaa693d84ae2a39fe5bbfda65add0b4f2a542f87
Adam Hamar <adam.hamar@gmail.com>
2023-06-17 10:12:08 +0200

    add active state to PlanCard

--- multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/PlanCard/PlanCard.tsx
+++ multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/PlanCard/PlanCard.tsx
@@ -3,14 +3,23 @@ type PlanCardProps = {
     note?: string;
     title: string;
     subtitle: string;
+    active: boolean;
 };
 
-function PlanCard({ image, note, title, subtitle }: PlanCardProps) {
+function PlanCard({ image, note, title, subtitle, active }: PlanCardProps) {
     const height = note ? "h-[183px]" : "h-[160px]";
+
+    const sectionVariants = {
+        active: "bg-ghost-white border-purple-navy",
+        inactive:
+            "bg-white border-gray-300 hover:bg-ghost-white hover:border-purple-navy",
+    };
 
     return (
         <section
-            className={`flex ${height} w-[8.5rem] shrink-0 grow flex-col rounded-md border border-gray-300 px-[16px] pb-[14px] pt-[20px]`}
+            className={`flex ${height} w-[8.5rem] shrink-0 grow cursor-pointer flex-col rounded-md border px-[16px] pb-[14px] pt-[20px] ${
+                sectionVariants[active ? "active" : "inactive"]
+            }`}
         >
             <img src={image} alt="" className="w-[42px]" />
             <p className="mt-auto text-[18px] font-bold text-marine-blue">
