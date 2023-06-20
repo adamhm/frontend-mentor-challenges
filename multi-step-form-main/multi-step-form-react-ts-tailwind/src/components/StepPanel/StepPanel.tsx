51551f802f30c65a6f4cc47a15f214a25b586c5e
Adam Hamar <adam.hamar@gmail.com>
2023-06-20 18:58:38 +0200

    remove context provider

--- multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/StepPanel/StepPanel.tsx
+++ multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/StepPanel/StepPanel.tsx
@@ -1,28 +1,22 @@
 import { steps } from "@shared/steps";
 import { StepElement } from "@components";
-import { useMemo, useState } from "react";
-import StepContext from "@contexts/step-context";
+import { useContext } from "react";
+import { StepContext } from "@contexts";
 
 function StepPanel() {
-    const [activeStep, setActiveStep] = useState(1);
-    const context = useMemo(
-        () => ({ activeStep, setActiveStep }),
-        [activeStep]
-    );
+    const { activeStep } = useContext(StepContext);
 
     return (
-        <StepContext.Provider value={context}>
-            <div className="flex h-full flex-col gap-y-[28px] px-[30px] py-[40px]">
-                {steps.map((step) => (
-                    <StepElement
-                        key={step.id}
-                        step={step.id}
-                        text={step.text}
-                        active={step.id === activeStep}
-                    />
-                ))}
-            </div>
-        </StepContext.Provider>
+        <div className="flex h-full flex-col gap-y-[28px] px-[30px] py-[40px]">
+            {steps.map((step) => (
+                <StepElement
+                    key={step.id}
+                    step={step.id}
+                    text={step.text}
+                    active={step.id === activeStep}
+                />
+            ))}
+        </div>
     );
 }
 
