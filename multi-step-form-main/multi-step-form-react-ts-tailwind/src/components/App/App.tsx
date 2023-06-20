b91485b46d288db2c27e32ca78c80d11f4c88e2c
Adam Hamar <adam.hamar@gmail.com>
2023-06-20 11:15:39 +0200

    add the step context

--- multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/App/App.tsx
+++ multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/App/App.tsx
@@ -1,20 +1,31 @@
 import { ContentPanel, Sidebar } from "@components";
-import { useMemo, useReducer } from "react";
+import { useMemo, useReducer, useState } from "react";
 import formReducer from "@state/form-reducer";
 import initState from "@state/init-state";
-import FormContext from "@contexts/form-context";
+import { FormContext, StepContext } from "@contexts";
 
 function App() {
     const [state, dispatch] = useReducer(formReducer, initState);
     const formContext = useMemo(() => ({ state, dispatch }), [state]);
 
+    const [activeStep, setActiveStep] = useState(1);
+    const stepContext = useMemo(
+        () => ({
+            activeStep,
+            setActiveStep,
+        }),
+        [activeStep]
+    );
+
     return (
-        <FormContext.Provider value={formContext}>
-            <article className="flex h-[600px] w-[940px] gap-4 rounded-2xl bg-white p-4 shadow-xl">
-                <Sidebar />
-                <ContentPanel />
-            </article>
-        </FormContext.Provider>
+        <StepContext.Provider value={stepContext}>
+            <FormContext.Provider value={formContext}>
+                <article className="flex h-[600px] w-[940px] gap-4 rounded-2xl bg-white p-4 shadow-xl">
+                    <Sidebar />
+                    <ContentPanel />
+                </article>
+            </FormContext.Provider>
+        </StepContext.Provider>
     );
 }
 
