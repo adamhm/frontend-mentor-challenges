7261a4c32a764553e604f79ccbc3f3c384c1b115
Adam Hamar <adam.hamar@gmail.com>
2023-07-01 11:22:33 +0200

    add mobile-view to the Step2 component

--- multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/Step2/Step2.tsx
+++ multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/Step2/Step2.tsx
@@ -32,36 +32,45 @@ function Step2() {
 
     return (
         <div className="flex h-full flex-col">
-            <StepTitleBar
-                title="Select your plan"
-                subtitle="You have the option of monthly or yearly billing."
-            />
-            <div className="mt-[34px] flex gap-[18px]">
-                {objectKeys(planData).map((plan) => (
-                    <PlanCard
-                        key={plan}
-                        image={`/src/assets/images/${planData[plan].image}`}
-                        title={plan}
-                        subtitle={`$${planData[plan][billing]}/${priceSuffix}`}
-                        note={
-                            billing === "yearly"
-                                ? planData[plan].note
-                                : undefined
-                        }
-                        active={state.plan === plan}
-                        onClick={() => handlePlanChange(plan)}
+            <div className=" px-[1rem] md:px-0">
+                <div className="mb-[24px] rounded-2xl border border-white bg-white px-[24px] pb-[1px] pt-[28px] shadow-xl md:h-[376px] md:px-0 md:pt-0 md:shadow-none">
+                    <StepTitleBar
+                        title="Select your plan"
+                        subtitle="You have the option of monthly or yearly billing."
                     />
-                ))}
+                    <div className="mt-[24px] flex flex-col gap-[12px] md:mt-[34px] md:flex-row md:gap-[18px]">
+                        {objectKeys(planData).map((plan) => (
+                            <PlanCard
+                                key={plan}
+                                image={`/src/assets/images/${planData[plan].image}`}
+                                title={plan}
+                                subtitle={`$${planData[plan][billing]}/${priceSuffix}`}
+                                note={
+                                    billing === "yearly"
+                                        ? planData[plan].note
+                                        : undefined
+                                }
+                                active={state.plan === plan}
+                                onClick={() => handlePlanChange(plan)}
+                            />
+                        ))}
+                    </div>
+                    <div className="my-6 flex h-[3rem] items-center justify-center bg-ghost-white md:mb-0 md:mt-9">
+                        <ToggleButton
+                            state={billing}
+                            onChange={handleBillingChange}
+                        />
+                    </div>
+                </div>
             </div>
-            <div className="mt-9 flex h-[3rem] items-center justify-center bg-ghost-white">
-                <ToggleButton state={billing} onChange={handleBillingChange} />
+            <div className="mt-auto flex h-[4.5rem] items-center bg-white px-[16px] md:px-0">
+                <NavigationBar
+                    colorClass="bg-marine-blue"
+                    hoverColorClass="hover:bg-[#174a8b]"
+                    onPrimaryClick={primaryClickHandler}
+                    onSecondaryClick={secondaryClickHandler}
+                />
             </div>
-            <NavigationBar
-                colorClass="bg-marine-blue"
-                hoverColorClass="hover:bg-[#174a8b]"
-                onPrimaryClick={primaryClickHandler}
-                onSecondaryClick={secondaryClickHandler}
-            />
         </div>
     );
 }
