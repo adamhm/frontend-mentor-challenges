9126398470aea35f10970bc9fe95b30b760ac595
Adam Hamar <adam.hamar@gmail.com>
2023-07-01 11:20:46 +0200

    Add mobile-view to the StepTitleBar

--- multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/StepTitleBar/StepTitleBar.tsx
+++ multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/StepTitleBar/StepTitleBar.tsx
@@ -5,8 +5,10 @@ type StepTitleBarProps = {
 
 function StepTitleBar({ title, subtitle }: StepTitleBarProps) {
     return (
-        <header className="mt-8">
-            <h1 className="text-[32px] font-bold text-marine-blue">{title}</h1>
+        <header className="md:mt-8">
+            <h1 className="text-[24px] font-bold text-marine-blue md:text-[32px]">
+                {title}
+            </h1>
             <h2 className="mt-1 text-cool-gray">{subtitle}</h2>
         </header>
     );
