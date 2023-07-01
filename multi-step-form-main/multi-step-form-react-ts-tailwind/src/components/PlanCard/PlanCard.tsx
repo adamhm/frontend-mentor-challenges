e01a053b646a784a1096f7f187440714137be31a
Adam Hamar <adam.hamar@gmail.com>
2023-07-01 11:22:13 +0200

    add mobile-view to the PlanCard

--- multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/PlanCard/PlanCard.tsx
+++ multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/PlanCard/PlanCard.tsx
@@ -15,7 +15,7 @@ function PlanCard({
     active,
     onClick,
 }: PlanCardProps) {
-    const height = note ? "h-[183px]" : "h-[160px]";
+    const height = note ? "h-[100px] md:h-[183px]" : "h-[76px] md:h-[160px]";
 
     const sectionVariants = {
         active: "bg-ghost-white border-purple-navy",
@@ -27,21 +27,23 @@ function PlanCard({
         <button
             type="button"
             aria-pressed={active}
-            className={`flex ${height} w-[8.5rem] shrink-0 grow cursor-pointer flex-col rounded-md border px-[16px] pb-[14px] pt-[20px] ${
+            className={`flex ${height} w-full shrink-0 grow cursor-pointer flex-row rounded-md border px-[16px] md:w-[8.5rem] md:flex-col md:pb-[14px] md:pt-[20px] ${
                 sectionVariants[active ? "active" : "inactive"]
             }`}
             onClick={onClick}
         >
-            <img src={image} alt="" className="w-[42px]" />
-            <p className="mt-auto text-[18px] font-bold text-marine-blue">
-                {title}
-            </p>
-            <p className="text-[15px] text-cool-gray">{subtitle}</p>
-            {note && (
-                <p className="text-[14px] font-normal text-marine-blue">
-                    {note}
+            <img src={image} alt="" className="mt-[16px] w-[42px] md:mt-0" />
+            <div className="ml-4 mt-4 flex flex-col items-start md:ml-0 md:mt-auto">
+                <p className="text-[18px] font-bold text-marine-blue">
+                    {title}
                 </p>
-            )}
+                <p className="text-[15px] text-cool-gray">{subtitle}</p>
+                {note && (
+                    <p className="text-[14px] font-normal text-marine-blue">
+                        {note}
+                    </p>
+                )}
+            </div>
         </button>
     );
 }
