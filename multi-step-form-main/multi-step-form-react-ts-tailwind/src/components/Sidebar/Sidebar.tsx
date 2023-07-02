33f1df35bac896cca7e6e47391d351fe02de162a
Adam Hamar <adam.hamar@gmail.com>
2023-07-02 09:51:50 +0200

    make bg-image to cover the Step Panel, close #8

--- multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/Sidebar/Sidebar.tsx
+++ multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/Sidebar/Sidebar.tsx
@@ -2,7 +2,7 @@ import { StepPanel } from "@components";
 
 function Sidebar() {
     return (
-        <section className="h-[172px] w-full shrink-0 bg-mobile bg-no-repeat md:h-[568px] md:w-[274px] md:bg-desktop">
+        <section className="h-[172px] w-full shrink-0 bg-mobile bg-cover bg-no-repeat md:h-[568px] md:w-[274px] md:bg-desktop">
             <StepPanel />
         </section>
     );
