5077da8edee7dfa053f8071776aa89c42ff86e3f
Adam Hamar <adam.hamar@gmail.com>
2023-07-02 09:54:05 +0200

    Make the AddonCard component mobile-friendly

--- multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/AddonCard/AddonCard.tsx
+++ multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/AddonCard/AddonCard.tsx
@@ -30,26 +30,28 @@ function AddonCard({
         <button
             type="button"
             aria-pressed={active}
-            className={`mb-4 flex h-[5em] w-full items-center rounded-md border text-left ${
+            className={`mb-4 flex h-[4em] w-full items-center rounded-md border text-left last-of-type:mb-6 md:h-[5em] ${
                 sectionVariants[active ? "active" : "inactive"]
             }`}
             onClick={() => onChange?.(!active)}
         >
             <span
                 aria-hidden
-                className={`mx-[24px] flex h-[22px] w-[22px] items-center rounded-md border border-gray-300 ${
+                className={`mx-[1rem] flex h-[22px] w-[22px] items-center rounded-md border border-gray-300 md:mx-[24px] ${
                     buttonVariants[active ? "active" : "inactive"]
                 }`}
             >
                 {active && <img src={iconCheckMark} alt="" className="ml-1" />}
             </span>
             <div>
-                <p className="text-[17px] font-medium text-marine-blue">
+                <p className="md: text-[14px] font-bold tracking-tighter text-marine-blue md:text-[17px] md:tracking-normal">
                     {title}
                 </p>
-                <p className="text-[15px] text-cool-gray">{subtitle}</p>
+                <p className="text-[13px] tracking-tighter text-cool-gray md:text-[15px] md:tracking-normal">
+                    {subtitle}
+                </p>
             </div>
-            <p className="ml-auto mr-[24px] text-[15px] font-medium text-slate-blue">
+            <p className="ml-auto mr-[1rem] text-[13px] font-medium text-slate-blue md:mr-[24px] md:text-[15px]">
                 {text}
             </p>
         </button>
