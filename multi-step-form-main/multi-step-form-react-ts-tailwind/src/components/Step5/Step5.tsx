d58165931d513be95bf00232dc37fc9b79b773da
Adam Hamar <adam.hamar@gmail.com>
2023-07-04 01:25:10 +0200

    make the Step5 component mobile-friendly

--- multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/Step5/Step5.tsx
+++ multi-step-form-main/multi-step-form-react-ts-tailwind/src/components/Step5/Step5.tsx
@@ -1,18 +1,25 @@
+import { StepContent } from "@components";
 import thankYouIcon from "@assets/images/icon-thank-you.svg";
 
 function Step5() {
     return (
-        <div className="flex h-full flex-col items-center justify-center">
-            <img src={thankYouIcon} alt="" />
-            <h1 className="mb-[0.2em] mt-[1em] text-[2em] font-bold text-marine-blue">
-                Thank you!
-            </h1>
-            <p className="mx-[-10px] text-center text-[17px] text-gray-400">
-                Thanks for confirming your subscription! We hope you have fun
-                using our platform. If you ever need support, please feel free
-                to email us at support@loremgaming.com.
-            </p>
-        </div>
+        <StepContent>
+            <div className="relative top-[-12px] flex h-[370px] flex-col items-center justify-center md:top-[98px]">
+                <img
+                    src={thankYouIcon}
+                    alt=""
+                    className="max-w-[56px] md:max-w-[100%]"
+                />
+                <h1 className="mb-[0.2em] mt-5 text-[24px] font-bold text-marine-blue md:mt-7 md:text-[2em]">
+                    Thank you!
+                </h1>
+                <p className="mx-[-10px] text-center text-[17px] text-gray-400">
+                    Thanks for confirming your subscription! We hope you have
+                    fun using our platform. If you ever need support, please
+                    feel free to email us at support@loremgaming.com.
+                </p>
+            </div>
+        </StepContent>
     );
 }
 
