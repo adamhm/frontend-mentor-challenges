8e73cb85abc1e92005ba7190075e5091e3e28f91
Adam Hamar <adam.hamar@gmail.com>
2023-07-08 11:07:39 +0200

    initial commit of the REST Countries API project

---
+++ rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/components/App/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+    return <div className="block text-red-600">App</div>;
+}
+
+export default App;
---
+++ rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import Unfonts from "unplugin-fonts/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: {
+            "@assets": path.resolve(__dirname, "./src/assets"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@contexts": path.resolve(__dirname, "./src/contexts"),
+            "@data": path.resolve(__dirname, "./src/data"),
+            "@shared": path.resolve(__dirname, "./src/shared"),
+            "@state": path.resolve(__dirname, "./src/state"),
+            "@typedefs": path.resolve(__dirname, "./src/typedefs"),
+            "@utils": path.resolve(__dirname, "./src/utils"),
+        },
+    },
+    plugins: [react()],
+});
