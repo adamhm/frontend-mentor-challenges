e5914a345334fd8fa31cd4ecf79b4ea54139591f
Adam Hamar <adam.hamar@gmail.com>
2023-07-08 12:11:39 +0200

    add colors to the tailwind config

--- rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/tailwind.config.js
+++ rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/tailwind.config.js
@@ -6,6 +6,13 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
+            colors: {
+                "outer-space": "hsl(209, 23%, 22%)",
+                "yankees-blue": "hsl(207, 26%, 17%)",
+                "chinese-black": "hsl(200, 15%, 8%)",
+                "old-silver": "hsl(0, 0%, 52%)",
+                lotion: "hsl(0, 0%, 98%)",
+            },
             fontFamily: {
                 sans: ["Nunito Sans", ...defaultTheme.fontFamily.sans],
             },
