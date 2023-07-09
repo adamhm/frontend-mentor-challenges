76953d8f344e2a205de595a6a3aa5c607b35b4fa
Adam Hamar <adam.hamar@gmail.com>
2023-07-09 12:24:52 +0200

    create the Home component

---
+++ rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import { SearchBox } from "@components";
+
+function Home() {
+    return (
+        <main className="px-[5rem] py-[3rem]">
+            <SearchBox />
+        </main>
+    );
+}
+
+export default Home;
