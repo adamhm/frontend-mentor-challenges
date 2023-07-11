cc2df69a67cc47021064fd5986c7833e5388e428
Adam Hamar <adam.hamar@gmail.com>
2023-07-12 01:55:13 +0200

    refine the style of the SearchBox

--- rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/components/SearchBox/SearchBox.tsx
+++ rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/components/SearchBox/SearchBox.tsx
@@ -5,7 +5,7 @@ import { ReactComponent as SearchIcon } from "@assets/search-outline.svg";
 function SearchBox() {
     return (
         <form className="block">
-            <div className="flex h-14 w-[30rem] items-center rounded-md border px-8 shadow-md">
+            <div className="flex h-14 w-[30rem] items-center rounded-md border px-8 shadow-[0px_0px_8px_rgba(240,240,240,0.6)]">
                 <label htmlFor="search" className="sr-only">
                     Search for a country
                 </label>
@@ -18,7 +18,7 @@ function SearchBox() {
                     name="search"
                     type="search"
                     placeholder="Search for a country..."
-                    className="ml-6 text-[14px] font-semibold placeholder-old-silver"
+                    className="ml-6 p-2 text-[14px] font-semibold placeholder-old-silver"
                 />
             </div>
         </form>
