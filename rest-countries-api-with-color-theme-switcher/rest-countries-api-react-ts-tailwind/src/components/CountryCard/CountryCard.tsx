6bae2b0a07a862e8e594d3e1432a829e08c4893a
Adam Hamar <adam.hamar@gmail.com>
2023-07-15 09:46:28 +0200

    refine the style of the CountryCard component

--- rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/components/CountryCard/CountryCard.tsx
+++ rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/components/CountryCard/CountryCard.tsx
@@ -9,14 +9,25 @@ function CountryCard({
 }: CountryCardProps) {
     return (
         <section className="h-[21rem] w-[16.5rem] bg-white shadow-[0px_0px_8px_rgba(240,240,240,0.6)]">
-                <strong>Population:</strong> {population}
-            </p>
-            <p>
-                <strong>Region:</strong> {region}
-            </p>
-            <p>
-                <strong>Capital:</strong> {capital}
-            </p>
+            <img
+                src={flag}
+                alt=""
+                className="h-[10rem] w-[16.5rem] object-cover"
+            />
+            <div className="p-6 text-[14px]">
+                <h2 className="text-[18px]">
+                    <strong>{name}</strong>
+                </h2>
+                <p className="mt-3">
+                    <strong>Population:</strong> {population}
+                </p>
+                <p>
+                    <strong>Region:</strong> {region}
+                </p>
+                <p>
+                    <strong>Capital:</strong> {capital}
+                </p>
+            </div>
         </section>
     );
 }
