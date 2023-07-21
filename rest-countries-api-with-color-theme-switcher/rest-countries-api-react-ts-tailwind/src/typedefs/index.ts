30ab4fa5ebe057d4cdfa7f049fb0c79ab6c6ae51
Adam Hamar <adam.hamar@gmail.com>
2023-07-21 10:08:44 +0200

    add type definitions for country details

--- rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/typedefs/index.ts
+++ rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/typedefs/index.ts
@@ -7,6 +7,27 @@ interface Country {
     [prop: string]: any;
 }
 
-interface CountryDetails extends Country {}
+type CountryName = {
+    common: string;
+    official: string;
+    nativeName: { [lang: string]: { official: string; common: string } };
+};
 
-export type { Country, CountryDetails };
+interface Country2 {
+    flags: { svg: string; [imgType: string]: string };
+    name: CountryName;
+    population: number;
+    region: string;
+    capital: string[];
+    [prop: string]: any;
+}
+
+interface CountryFullInfo extends Country2 {
+    subregion: string;
+    tld: Array<string>;
+    currencies: { [currency: string]: { name: string; symbol: string } };
+    languages: { [language: string]: string };
+    borders: Array<string>;
+}
+
+export type { Country, CountryFullInfo, CountryName };
