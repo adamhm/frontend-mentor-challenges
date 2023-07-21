5dad3375f313efba11cb5c9d9bc2b3b5c871b414
Adam Hamar <adam.hamar@gmail.com>
2023-07-21 10:04:32 +0200

    change the getCountry func to be type-safe

--- rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/api/getCountry.ts
+++ rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/api/getCountry.ts
@@ -1,9 +1,13 @@
-async function getCountry(countryCode: string) {
+import { QueryFunctionContext } from "@tanstack/react-query";
+import { CountryFullInfo } from "@typedefs";
+
+async function getCountry({ queryKey: [, countryCode] }: QueryFunctionContext) {
     const response = await fetch(
         `https://restcountries.com/v3.1/alpha/${countryCode}`
     );
+    const data: CountryFullInfo[] = await response.json();
 
-    return response.json();
+    return Promise.resolve(data[0]);
 }
 
 export default getCountry;
