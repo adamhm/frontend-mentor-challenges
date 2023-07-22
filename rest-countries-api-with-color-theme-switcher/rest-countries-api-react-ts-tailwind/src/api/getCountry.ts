cda525ca5a4dbf8a5780538a647bb529d000538c
Adam Hamar <adam.hamar@gmail.com>
2023-07-22 10:48:21 +0200

    refactor the API-functions to be more type safe

--- rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/api/getCountry.ts
+++ rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/api/getCountry.ts
@@ -1,13 +1,15 @@
-import { QueryFunctionContext } from "@tanstack/react-query";
+import { QueryFunction } from "@tanstack/react-query";
 import { CountryFullInfo } from "@typedefs";
 
-async function getCountry({ queryKey: [, countryCode] }: QueryFunctionContext) {
+const getCountry2: QueryFunction<CountryFullInfo[], [string, string]> = async ({
+    queryKey,
+}) => {
+    const [, countryCode] = queryKey;
     const response = await fetch(
         `https://restcountries.com/v3.1/alpha/${countryCode}`
     );
-    const data: CountryFullInfo[] = await response.json();
 
-    return Promise.resolve(data[0]);
-}
+    return response.json() as Promise<CountryFullInfo[]>;
+};
 
-export default getCountry;
+export default getCountry2;
--- rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/api/getCountryNames.ts
+++ rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/api/getCountryNames.ts
@@ -1,17 +1,24 @@
+import { QueryFunction } from "@tanstack/react-query";
 import { CountryName } from "@typedefs";
 
-async function getCountryNames(
-    countryCodes: string[] | undefined
-): Promise<{ name: CountryName }[]> {
+const getCountryNames2: QueryFunction<
+    { name: CountryName; cioc: string }[],
+    [string, string[] | undefined]
+> = async ({ queryKey }) => {
+    const [, countryCodes] = queryKey;
+
     if (!countryCodes)
-        return Promise.resolve([]) as Promise<{ name: CountryName }[]>;
+        return Promise.resolve([]) as Promise<
+            { name: CountryName; cioc: string }[]
+        >;
 
     const response = await fetch(
         `https://restcountries.com/v3.1/alpha?codes=${countryCodes.join(
             ","
         )}&fields=name,cioc`
     );
-    return response.json() as Promise<{ name: CountryName }[]>;
-}
 
-export default getCountryNames;
+    return response.json() as Promise<{ name: CountryName; cioc: string }[]>;
+};
+
+export default getCountryNames2;
