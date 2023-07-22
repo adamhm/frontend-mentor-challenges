e730f7935b89a9a8079611ce78938d91803d880c
Adam Hamar <adam.hamar@gmail.com>
2023-07-22 11:09:34 +0200

    replace the hard coded data with API-calls

--- rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/components/CountryDetails/CountryDetails.tsx
+++ rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/components/CountryDetails/CountryDetails.tsx
@@ -1,75 +1,111 @@
+import { useQuery } from "@tanstack/react-query";
+import getCountry from "@api/getCountry";
+import getCountryNames from "@api/getCountryNames";
+
 type CountryDetailsProps = {
     countryCode: string;
+    onChange: (countryCode: string | null) => void;
 };
 
-function CountryDetails({ countryCode }: CountryDetailsProps) {
+function CountryDetails({ countryCode, onChange }: CountryDetailsProps) {
+    const {
+        data: country,
+        isError: isCountryError,
+        isLoading: isCountryLoading,
+    } = useQuery({
+        queryKey: ["fullInfo", countryCode],
+        queryFn: getCountry,
+        select: (countries) => countries[0],
+    });
+
+    const {
+        data: borders = [],
+        isError: isBordersError,
+        isLoading: isBordersLoading,
+    } = useQuery({
+        queryKey: ["borders", country?.borders],
+        queryFn: getCountryNames,
+        enabled: !!country,
+    });
+
+    if (isCountryLoading || isBordersLoading) {
+        return null;
+    }
+
+    if (isCountryError || isBordersError) return <div>Error</div>;
+
     return (
         <section className="">
-            <button type="button">Back</button>
+            <button type="button" onClick={() => onChange(null)}>
+                Back
+            </button>
             <div className="flex items-center">
                 <img
-                    src="https://flagcdn.com/be.svg"
+                    src={country.flags.svg}
                     alt=""
                     className="h-[400px] w-[560px]"
                 />
                 <div className="ml-[120px] w-full">
-                    <h2 className="mb-6 text-[2rem] font-bold">Belgium</h2>
+                    <h2 className="mb-6 text-[2rem] font-bold">
+                        {country.name.common}
+                    </h2>
                     <div className="flex">
                         <div>
                             <p>
                                 <strong>Native Name: </strong>
-                                "Belgie"
+                                {
+                                    Object.values(country.name.nativeName)[0]
+                                        .common
+                                }
                             </p>
                             <p className="mt-2">
                                 <strong>Population: </strong>
-                                11,319,511
+                                {country.population.toLocaleString("en-us")}
                             </p>
                             <p className="mt-2">
                                 <strong>Region: </strong>
-                                Europe
+                                {country.region}
                             </p>
                             <p className="mt-2">
                                 <strong>Sub Region: </strong>
-                                Western Europe
+                                {country.subregion}
                             </p>
                             <p className="mt-2">
                                 <strong>Capital: </strong>
-                                Brussels
+                                {country.capital.join(", ")}
                             </p>
                         </div>
                         <div className="ml-auto">
                             <p>
-                                <strong>Top Level Domain: </strong>.be
+                                <strong>Top Level Domain: </strong>
+                                {country.tld.join(", ")}
                             </p>
                             <p className="mt-2">
-                                <strong>Currencies: </strong>Euro
+                                <strong>Currencies: </strong>
+                                {Object.keys(country.currencies).join(", ")}
                             </p>
                             <p className="mt-2">
-                                <strong>Languages: </strong>Dutch, French,
-                                German
+                                <strong>Languages: </strong>
+                                {Object.values(country.languages).join(", ")}
                             </p>
                         </div>
                     </div>
-                    <div className="mt-[5rem]">
-                        <strong className="mr-4">Border Countries:</strong>
-                        <button
-                            type="button"
-                            className="mr-2 h-8 rounded border-2 border-gray-200 px-7 text-[14px]"
-                        >
-                            France
-                        </button>
-                        <button
-                            type="button"
-                            className="mr-2 h-8 rounded border-2 border-gray-200 px-7 text-[14px]"
-                        >
-                            Germany
-                        </button>
-                        <button
-                            type="button"
-                            className="h-8 rounded border-2 border-gray-200 px-7 text-[14px]"
-                        >
-                            Netherlands
-                        </button>
+                    <div className="mt-[5rem] flex">
+                        <strong className="mr-4 shrink-0">
+                            Border Countries:
+                        </strong>
+                        <div className="flex flex-wrap gap-2">
+                            {borders.map((border) => (
+                                <button
+                                    type="button"
+                                    key={border.cioc}
+                                    className="h-8 rounded border-2 border-gray-200 px-7 text-[14px]"
+                                    onClick={() => onChange(border.cioc)}
+                                >
+                                    {border.name.common}
+                                </button>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
