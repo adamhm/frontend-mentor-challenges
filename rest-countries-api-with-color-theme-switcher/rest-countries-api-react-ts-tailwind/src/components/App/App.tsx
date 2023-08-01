a6e8452be1991c3b7bfb82576480a00f8605c15c
Adam Hamar <adam.hamar@gmail.com>
2023-08-01 15:52:14 +0200

    add React Query Devtools to the project

--- rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/components/App/App.tsx
+++ rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import { Header, Home } from "@components";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
 function App() {
     return (
         <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false} />
             <Header />
             <Home />
         </QueryClientProvider>
