fc1c10e3d2c709ae1b79d160b02c43ae83e1e777
Adam Hamar <adam.hamar@gmail.com>
2023-08-08 09:40:42 +0200

    refactor schemas using the extend method

--- rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/schema/index.ts
+++ rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/schema/index.ts
@@ -18,47 +18,31 @@ const countryBaseSchema = z.object({
     cca3: z.string(),
 });
 
-const countrySchema = z.record(z.any()).and(
-    z.object({
-        name: countryNameSchema,
-        cca3: z.string(),
-        flags: z.record(z.string()).and(
-            z.object({
-                svg: z.string(),
-            })
-        ),
-        population: z.number(),
-        region: z.string(),
-        capital: z.optional(z.array(z.string())),
-    })
-);
+const countrySchema = countryBaseSchema.extend({
+    flags: z.record(z.string()).and(
+        z.object({
+            svg: z.string(),
+        })
+    ),
+    population: z.number(),
+    region: z.string(),
+    capital: z.optional(z.array(z.string())),
+});
 
-const countryFullInfoSchema = z.record(z.any()).and(
-    z.object({
-        name: countryNameSchema,
-        cca3: z.string(),
-        flags: z.record(z.string()).and(
+const countryFullInfoSchema = countrySchema.extend({
+    subregion: z.optional(z.string()),
+    tld: z.optional(z.array(z.string())),
+    currencies: z.optional(
+        z.record(
             z.object({
-                svg: z.string(),
+                name: z.string(),
+                symbol: z.optional(z.string()),
             })
-        ),
-        population: z.number(),
-        region: z.string(),
-        capital: z.optional(z.array(z.string())),
-        subregion: z.optional(z.string()),
-        tld: z.optional(z.array(z.string())),
-        currencies: z.optional(
-            z.record(
-                z.object({
-                    name: z.string(),
-                    symbol: z.string(),
-                })
-            )
-        ),
-        languages: z.optional(z.record(z.string())),
-        borders: z.optional(z.array(z.string())),
-    })
-);
+        )
+    ),
+    languages: z.optional(z.record(z.string())),
+    borders: z.optional(z.array(z.string())),
+});
 
 export {
     countryBaseSchema,
