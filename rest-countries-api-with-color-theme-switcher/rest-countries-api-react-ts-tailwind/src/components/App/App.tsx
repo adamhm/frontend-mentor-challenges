3d1336691b5e6a1147546079bee0f75937d1b114
Adam Hamar <adam.hamar@gmail.com>
2023-08-09 23:48:25 +0200

    add default caching options

--- rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/components/App/App.tsx
+++ rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/components/App/App.tsx
@@ -2,7 +2,14 @@ import { Header, Home } from "@components";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 5 * 60 * 1000,
+            cacheTime: 10 * 60 * 1000,
+        },
+    },
+});
 
 function App() {
     return (
