eeea6f4574a7b3cba14e6859377ad20514e4124a
Adam Hamar <adam.hamar@gmail.com>
2023-08-18 12:06:31 +0200

    create the InfoPanel component

---
+++ ecommerce-product-page-main/src/components/InfoPanel/InfoPanel.tsx
@@ -0,0 +1,19 @@
+function InfoPanel() {
+    return (
+        <section className="w-6/12 border pl-[62px]">
+            <p className="text-[12px] font-bold uppercase text-orange">
+                Sneaker Company
+            </p>
+            <h1 className="text-[2.5rem] font-bold leading-[44px]">
+                Fall Limited Edition Sneakers
+            </h1>
+            <p>
+                These low-profile sneakers are your perfect casual wear
+                companion. Featuring a durable rubber outer sole, they’ll
+                withstand everything the weather can offer.
+            </p>
+        </section>
+    );
+}
+
+export default InfoPanel;
