bb645ff3c5d00458390b8c6b245ee3614fd53b82
Adam Hamar <adam.hamar@gmail.com>
2023-08-20 11:29:15 +0200

    create the QuantitySelector component

---
+++ ecommerce-product-page-main/src/components/QuantitySelector/QuantitySelector.tsx
@@ -0,0 +1,28 @@
+import { ReactComponent as MinusIcon } from "@assets/icon-minus.svg";
+import { ReactComponent as PlusIcon } from "@assets/icon-plus.svg";
+
+type QuantitySelectorProps = {
+    value: number;
+    onPlusClick?: () => void;
+    onMinusClick?: () => void;
+};
+
+function QuantitySelector({
+    value,
+    onMinusClick,
+    onPlusClick,
+}: QuantitySelectorProps) {
+    return (
+        <section className="flex h-[54px] w-[156px] items-center justify-between rounded-lg bg-light-grayish-blue px-4">
+            <button type="button" onClick={onMinusClick}>
+                <MinusIcon />
+            </button>
+            <span className="text-[15px] font-bold">{value}</span>
+            <button type="button" onClick={onPlusClick}>
+                <PlusIcon />
+            </button>
+        </section>
+    );
+}
+
+export default QuantitySelector;
--- ecommerce-product-page-main/src/components/index.ts
+++ ecommerce-product-page-main/src/components/index.ts
@@ -2,5 +2,6 @@ import Header from "./Header/Header";
 import ImagePanel from "./ImagePanel/ImagePanel";
 import InfoPanel from "./InfoPanel/InfoPanel";
 import MainContainer from "./MainContainer/MainContainer";
+import QuantitySelector from "./QuantitySelector/QuantitySelector";
 
-export { Header, ImagePanel, InfoPanel, MainContainer };
+export { Header, ImagePanel, InfoPanel, MainContainer, QuantitySelector };
