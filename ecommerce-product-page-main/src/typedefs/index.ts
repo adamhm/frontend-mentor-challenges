ab8ee57a3d67ca9fe08613d0b7f3c352e551c0ad
Adam Hamar <adam.hamar@gmail.com>
2023-08-21 10:56:26 +0200

    define types for cart context

--- ecommerce-product-page-main/src/typedefs/index.ts
+++ ecommerce-product-page-main/src/typedefs/index.ts
@@ -1,3 +1,18 @@
 import data from "@data/products.json";
 
-export type Product = (typeof data.products)[number];
+type Product = (typeof data.products)[number];
+
+type CartItem = { product: Product; quantity: number };
+
+type CartState = { items: Array<CartItem> };
+
+type CartAction =
+    | { type: "ADD"; payload: CartItem }
+    | { type: "REMOVE"; payload: number };
+
+type CartContextType = {
+    state: CartState;
+    dispatch: React.Dispatch<CartAction>;
+};
+
+export type { CartContextType, CartAction, CartState, Product };
