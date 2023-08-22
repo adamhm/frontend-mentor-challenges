fbd9c06d69cf024fbcd1b97670ef6eb423295914
Adam Hamar <adam.hamar@gmail.com>
2023-08-22 11:51:22 +0200

    update the cart item when add item more than once

--- ecommerce-product-page-main/src/state/cart-reducer.ts
+++ ecommerce-product-page-main/src/state/cart-reducer.ts
@@ -3,6 +3,28 @@ import type { CartAction, CartState } from "@typedefs";
 function cartReducer(state: CartState, action: CartAction) {
     switch (action.type) {
         case "ADD":
+            const itemIndex = state.items.findIndex(
+                (item) => item.product.id === action.payload.product.id
+            );
+
+            if (itemIndex > -1) {
+                const newItem = {
+                    product: state.items[itemIndex].product,
+                    quantity:
+                        state.items[itemIndex].quantity +
+                        action.payload.quantity,
+                };
+
+                return {
+                    ...state,
+                    items: [
+                        ...state.items.slice(0, itemIndex),
+                        newItem,
+                        ...state.items.slice(itemIndex + 1),
+                    ],
+                };
+            }
+
             return { ...state, items: [...state.items, action.payload] };
         case "REMOVE":
             return {
