65e99082ecd4eb02d2ee15553e45275889448578
Adam Hamar <adam.hamar@gmail.com>
2023-09-22 11:52:31 +0200

    add animation to the dropdown

--- rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/components/Dropdown/Dropdown.tsx
+++ rest-countries-api-with-color-theme-switcher/rest-countries-api-react-ts-tailwind/src/components/Dropdown/Dropdown.tsx
@@ -1,3 +1,5 @@
+import { AnimatePresence, motion } from "framer-motion";
+
 import { ReactComponent as DownIcon } from "@assets/chevron-down-outline.svg";
 import { Region } from "@typedefs";
 import useDropdown from "@hooks/useDropdown";
@@ -21,21 +23,42 @@ function Dropdown({ selectedItem, items, onChange }: DropdownProps) {
                 {selectedItem ?? "Filter by Region"}
                 <DownIcon className="ml-auto h-4 w-4" />
             </button>
-            {isOpen && (
-                <menu className="absolute mt-1 w-[200px] rounded-md border bg-white py-4 text-[14px] font-semibold shadow-[0px_0px_8px_rgba(240,240,240,0.6)] dark:border-outer-space dark:bg-outer-space dark:text-white dark:shadow-none">
-                    {items.map((item) => (
-                        <li key={item}>
-                            <button
-                                type="button"
-                                className="w-full px-6 py-1 text-left hover:bg-gray-200"
-                                onClick={clickHandler}
+            <AnimatePresence>
+                {isOpen && (
+                    <motion.menu
+                        className="absolute mt-1 w-[200px] rounded-md border bg-white py-4 text-[14px] font-semibold shadow-[0px_0px_8px_rgba(240,240,240,0.6)] dark:border-outer-space dark:bg-outer-space dark:text-white dark:shadow-none"
+                        variants={{
+                            hidden: { opacity: 0, scale: 0.5 },
+                            visible: {
+                                opacity: 1,
+                                scale: 1,
+                                transition: { staggerChildren: 0.1 },
+                            },
+                        }}
+                        initial="hidden"
+                        animate="visible"
+                        exit="hidden"
+                    >
+                        {items.map((item) => (
+                            <motion.li
+                                key={item}
+                                variants={{
+                                    hidden: { opacity: 0, scale: 0.5 },
+                                    visible: { opacity: 1, scale: 1 },
+                                }}
                             >
-                                {item}
-                            </button>
-                        </li>
-                    ))}
-                </menu>
-            )}
+                                <button
+                                    type="button"
+                                    className="w-full px-6 py-1 text-left hover:bg-gray-200"
+                                    onClick={clickHandler}
+                                >
+                                    {item}
+                                </button>
+                            </motion.li>
+                        ))}
+                    </motion.menu>
+                )}
+            </AnimatePresence>
         </div>
     );
 }
